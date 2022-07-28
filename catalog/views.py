5ba0a2d4b557efebbbc745572511ee5f09e602ca
rzfeeser <rzfeeser@gmail.com>
2022-07-28 04:25:20 +0000

    sessions completed

--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 from .models import Book, Author, BookInstance, Genre
 
+
+
 def index(request):
     """View function for home page of site."""
 
@@ -14,18 +16,25 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
-    # The 'all()' is implied by default.
-    num_authors = Author.objects.count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
-    # Render the HTML template index.html with the data in the context variable
+    # Render the HTML template index.html with the data in the context variable.
     return render(request, 'index.html', context=context)
+
+
+
 
 
 from django.views import generic
